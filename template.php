7f1616ed8b28c6ee4b2a0f47ea4b511ec74f1e29
scito <info@scito.ch>
2014-06-30 09:24:49 +0200

    Improve node dates, similar to bergamotte

--- template.php
+++ template.php
@@ -25,6 +25,53 @@ function transparent_sky_preprocess_page(&$variables) {
 }
 
 /**
+ * Override or insert variables into the node templates.
+ */
+function transparent_sky_preprocess_node(&$vars) {
+  $node = $vars['node'];
+  // From node.module template_preprocess_node
+  $vars['date'] = format_date($node->created, 'short_date');
+
+  //
+  // AT Core builds additional time and date variables for use in templates
+  //
+  // datetime stamp formatted correctly to ISO8601
+//   $vars['datetime'] = format_date($vars['created'], 'custom', 'Y-m-d\TH:i:sO'); // PHP 'c' format is not proper ISO8601!
+
+  // Publication date, formatted with time element
+//   $vars['publication_date'] = '<time datetime="' . $vars['datetime'] . '" pubdate="pubdate">' . $vars['date'] . '</time>';
+
+  // Last update variables
+//   $vars['datetime_updated'] = format_date($vars['node']->changed, 'custom', 'Y-m-d\TH:i:sO');
+//   $vars['custom_date_and_time'] = date('jS F, Y - g:ia', $vars['node']->changed);
+
+  // Last updated formatted in time element
+//   $vars['last_update'] = '<time datetime="' . $vars['datetime_updated'] . '" pubdate="pubdate">' . $vars['custom_date_and_time'] . '</time>';
+
+  // Build the submitted variable used by default in node templates
+  if (variable_get('node_submitted_' . $vars['node']->type, TRUE)) {
+	$param = array(
+	  '!username' => $vars['name'],
+	  '!datetime' => '<time datetime="' . $vars['datetime'] . '" pubdate="pubdate">' . l(format_date($node->created, 'short_date'), 'node/'. $node->nid, array('alias' => TRUE, 'attributes' => array('rel' => 'bookmark'))) . '</time>',
+	  '!modified' => '<time datetime="' . $vars['datetime_updated'] . '">' . format_date($node->changed, 'short_date') . '</time>'
+	);
+
+	$vars['submitted'] = format_date($node->created, 'short_date') == format_date($node->changed, 'short_date') ? t("!username – !datetime", $param) : t("!username – !datetime (modified on !modified)", $param);
+//     $vars['submitted'] = t("!datetime (modified on !modified) – !username",
+//       array(
+//         '!username' => $vars['name'],
+//         '!datetime' => $vars['publication_date'],
+//       )
+//     );
+  }
+  else {
+    $vars['submitted'] = '';
+  }
+
+}
+
+
+/**
  * Override or insert variables for the html template.
  */
 /* -- Delete this line if you want to use this function
