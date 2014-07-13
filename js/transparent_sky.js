b5967112c4ddd9f684e907f0ab48fb51b6cc0237
scito <info@scito.ch>
2014-07-13 05:03:07 +0200

    Fix autocomplete auto submit issue (it was not automatic)

--- js/transparent_sky.js
+++ js/transparent_sky.js
@@ -230,6 +230,51 @@
     };
   });
 
+// Copied from misc/autocomplete.js
+// Fixed autocomplete auto submit, ref https://www.drupal.org/node/309088
+/**
+ * Handler for the "keyup" event.
+ */
+Drupal.jsAC.prototype.onkeyup = function (input, e) {
+  if (!e) {
+    e = window.event;
+  }
+  switch (e.keyCode) {
+    case 16: // Shift.
+    case 17: // Ctrl.
+    case 18: // Alt.
+    case 20: // Caps lock.
+    case 33: // Page up.
+    case 34: // Page down.
+    case 35: // End.
+    case 36: // Home.
+    case 37: // Left arrow.
+    case 38: // Up arrow.
+    case 39: // Right arrow.
+    case 40: // Down arrow.
+      return true;
+
+    case 9:  // Tab.
+    case 13: // Enter.
+    case 27: // Esc.
+      this.hidePopup(e.keyCode);
+      // ibex, ref https://www.drupal.org/node/309088
+      if (13 == e.keyCode && $(input).hasClass('auto_submit')) {
+        input.form.submit();
+      }
+      return true;
+
+    default: // All other keys.
+      if (input.value.length > 0 && !input.readOnly) {
+        this.populatePopup();
+      }
+      else {
+        this.hidePopup(e.keyCode);
+      }
+      return true;
+  }
+};
+
   })(jQuery);
 
 
